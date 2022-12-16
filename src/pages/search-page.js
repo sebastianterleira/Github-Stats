46b875879609edba506390935a8a457fb494dc34
sebastianterleira <seb.terleira1204@gmail.com>
2022-12-16 15:47:01 -0500

    WIP

--- src/pages/search-page.js
+++ src/pages/search-page.js
@@ -5,25 +5,25 @@ import GithubData from "./components/github-data";
 function SearchPage() {
   const [data, setData] = useState("");
   const [query, setQuery] = useState("");
-
-	const [state, setState] = useState({
-    status: "idle", // success - error - pending
-    data: null,
-    error: null,
-  });
-  const { status, data: pokemon, error } = state;
-
+  
 	useEffect(() => {
     getGithubUser(query)
-		.then((setData) => {
-			setState({ status: "pending", data: null, error: null })
-		})
-		.catch((error) => {
-			setState({
-          status: "error",
-          data: null,
-          error: "El usuario no existe",
-		})
-  })
+		.then(setData)
+		.catch(console.log) 
 	}, [query]);
+
+	return (
+		<div className="grid">
+			<div>
+          <input onChange={(event) => setQuery(event.target.value)} placeholder="Ingresa el nombre de usuario" />
+      </div>
+			<article>
+        {data && (
+          <GithubData github={data}></GithubData>
+        )}
+      </article>
+		</div>
+	);
 }
+
+export default SearchPage;
